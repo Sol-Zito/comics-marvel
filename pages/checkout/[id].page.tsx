2bbe607081df64043022f6ac78e0024bc876e797
Sol <solzito.1889@gmail.com>
2024-03-26 16:55:34 -0300

    se refactoriza el componente para forms

--- pages/checkout/[id].page.tsx
+++ pages/checkout/[id].page.tsx
@@ -4,7 +4,9 @@ import { getComic } from "dh-marvel/services/marvel/marvel.service";
 import { Grid, Typography } from "@mui/material";
 import { Comics } from "dh-marvel/features/marvel/comics.type";
 import ComicCheckout from "dh-marvel/components/comicCard/ComicCheckout";
+import { FormPrincipal } from "dh-marvel/components/form/FormPrincipal";
 import LayoutCheckout from "dh-marvel/components/layouts/layout-checkout";
+import BodySingle from "dh-marvel/components/layouts/body/single/body-single";
 
 interface Props {
   comic: Comics;
@@ -14,46 +16,45 @@ interface Props {
 const CheckoutPage: NextPage<Props> = ({ comic, id }) => {
   return (
     <LayoutCheckout>
-      <Typography variant="h4" align="center" mt={2}>
-        Checkout:
-      </Typography>
-      <Grid container spacing={2} columnSpacing={{ xs: 1, sm: 2, md: 4 }}>
-        <Grid
-          item
-          xs={12}
-          md={4}
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <ComicCheckout {...comic} />
+      <BodySingle title="Checkout:">
+        <Grid container spacing={2} columnSpacing={{ xs: 1, sm: 2, md: 4 }}>
+          <Grid
+            item
+            xs={12}
+            md={4}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <ComicCheckout {...comic} />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={8}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "5px",
+            }}
+          >
+            {comic?.stock > 0 ? (
+              <>
+                <FormPrincipal {...comic} />
+              </>
+            ) : (
+              <Typography variant="h4" align="center" mt={2}>
+                Sorry, there isn't stock available at the moment...
+              </Typography>
+            )}
+          </Grid>
         </Grid>
-        <Grid
-          item
-          xs={12}
-          md={8}
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            gap: "5px",
-          }}
-        >
-          {/* {comic?.stock > 0 ? (
-            <FormProvider {...methods}>
-              <Forms onSubmit={onSubmit} />
-            </FormProvider>
-          ) : (
-            <Typography variant="h4" align="center" mt={2}>
-             Sorry, there isn't stock available at the moment...
-            </Typography>
-          )} */}
-        </Grid>
-      </Grid>
+      </BodySingle>
     </LayoutCheckout>
   );
 };
